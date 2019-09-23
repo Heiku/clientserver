04cfc0353dfb680c8d5569245f6955e3d64bf68d
linjiahong <heiiku@qq.com>
2019-09-23 22:27:32 +0800

    9.23 输入参数检查

--- src/main/java/com/ljh/clientdemo/console/impl/user/RegisterConsoleCommand.java
+++ src/main/java/com/ljh/clientdemo/console/impl/user/RegisterConsoleCommand.java
@@ -12,30 +12,34 @@ public class RegisterConsoleCommand implements ConsoleCommand {
 
     @Override
     public void exec(Scanner scanner, Channel channel) {
-        // 读取输入
-        System.out.println("输入用户名，密码登录：(username,password)");
-        String accountInfo = scanner.next();
-        if (Strings.isNullOrEmpty(accountInfo)){
-            System.out.println("尚未输入账号信息，请重新发送 register 指令");
-            return;
+        try {
+            // 读取输入
+            System.out.println("输入用户名，密码登录：(username,password)");
+            String accountInfo = scanner.next();
+            if (Strings.isNullOrEmpty(accountInfo)){
+                System.out.println("尚未输入账号信息，请重新发送 register 指令");
+                return;
+            }
+
+            // 解析输入的字符串，分割为两个参数
+            String[] strArr = accountInfo.split(",");
+            String username = strArr[0];
+            String password = strArr[1];
+            if (Strings.isNullOrEmpty(username) || Strings.isNullOrEmpty(password)){
+                System.out.println("账号名或密码为空，请重新发送 register 指令");
+                return;
+            }
+
+            MsgUserInfoProto.RequestUserInfo requestUserInfo = MsgUserInfoProto.RequestUserInfo.newBuilder()
+                    .setRequestId(UUID.randomUUID().toString())
+                    .setType(MsgUserInfoProto.RequestType.REGISTER)
+                    .setUsername(username)
+                    .setPassword(password)
+                    .build();
+
+            channel.writeAndFlush(requestUserInfo);
+        } catch (Exception e) {
+            System.out.println("参数输入有误！");
         }
-
-        // 解析输入的字符串，分割为两个参数
-        String[] strArr = accountInfo.split(",");
-        String username = strArr[0];
-        String password = strArr[1];
-        if (Strings.isNullOrEmpty(username) || Strings.isNullOrEmpty(password)){
-            System.out.println("账号名或密码为空，请重新发送 register 指令");
-            return;
-        }
-
-        MsgUserInfoProto.RequestUserInfo requestUserInfo = MsgUserInfoProto.RequestUserInfo.newBuilder()
-                .setRequestId(UUID.randomUUID().toString())
-                .setType(MsgUserInfoProto.RequestType.REGISTER)
-                .setUsername(username)
-                .setPassword(password)
-                .build();
-
-        channel.writeAndFlush(requestUserInfo);
     }
 }
